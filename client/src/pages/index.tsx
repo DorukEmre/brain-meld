23bc627eac19f80aa0f442187aac531481e44eaf
DorukEmre <doruk.emre@outlook.com>
2023-02-16 23:32:38 +0000

    User can make requests and responses displayed

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,21 +1,46 @@
 import { gql, useQuery } from '@apollo/client'
 import type { NextPageWithLayout } from '@/pages/_app'
 import HTMLMetaTags from '@/components/HTMLMetaTags'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { ApolloClient, InMemoryCache } from '@apollo/client'
+
+import TextareaAutosize from '@mui/base/TextareaAutosize'
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
   cache: new InMemoryCache(),
 })
 
-type Props = {}
-const Home: NextPageWithLayout<Props> = (props) => {
+const Home: NextPageWithLayout = () => {
   const [input, setInput] = useState('')
-  const [response, setResponse] = useState('')
+  const [responses, setResponses] = useState<string[]>([])
+  const [isCopied, setIsCopied] = useState(false)
+  const [copiedIndex, setCopiedIndex] = useState(-1)
 
-  const handleSubmit = async (e: React.SyntheticEvent) => {
+  const handleSubmit = async (e: React.SyntheticEvent, index: number) => {
     e.preventDefault()
+
+    let context
+    if (
+      responses.length >= 3 &&
+      index !== responses.length - 1 &&
+      index !== responses.length - 2
+    ) {
+      context = responses.slice(-2).join('\n')
+    } else if (
+      responses.length >= 3 &&
+      (index === responses.length - 1 || index === responses.length - 2)
+    ) {
+      context = [responses[index - 2], responses[index - 1]].join('\n')
+    } else if (responses.length === 2 && index === 1) {
+      context = responses[0]
+    } else {
+      context = ''
+    }
+
+    const prompt = index === -1 ? input : responses[index]
+    const inputWithContext =
+      context !== '' ? [context, prompt].join('\n') : prompt
 
     const { data } = await client.query({
       query: gql`
@@ -23,31 +48,83 @@ const Home: NextPageWithLayout<Props> = (props) => {
           generateText(input: $input)
         }
       `,
-      variables: { input },
+      variables: { input: inputWithContext },
     })
 
-    setResponse(data.generateText)
+    console.log(inputWithContext)
+    console.log(data.generateText)
+    const sanitizedResponse = data.generateText.replace(/^\s*[\r\n]{2}/, '')
+
+    if (responses.length === 0) {
+      setResponses([prompt, sanitizedResponse])
+    } else if (index === -1) {
+      setResponses([...responses, input, sanitizedResponse])
+    } else {
+      setResponses([...responses, sanitizedResponse])
+    }
+
+    setInput('')
+  }
+
+  const handleCopy = (e: React.SyntheticEvent, index: number) => {
+    navigator.clipboard.writeText(responses[index]).then(() => {
+      setIsCopied(true)
+      setCopiedIndex(index)
+      setTimeout(() => {
+        setIsCopied(false)
+        setCopiedIndex(-1)
+      }, 2000)
+    })
   }
 
   return (
     <>
       <HTMLMetaTags />
-      <h1>Main content</h1>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Text:
-            <input
-              type="text"
-              value={input}
-              onChange={(event) => {
-                setInput(event.target.value)
-              }}
-            />
-          </label>
-          <button type="submit">Generate</button>
-        </form>
-        {response && <p>{response}</p>}
+      <div className="chatbox-container">
+        <div className="chatbox-conversation">
+          {responses.length > 0 &&
+            responses.map((response, index) => (
+              <div key={index} className="chatbox-response">
+                <form onSubmit={(e) => handleSubmit(e, index)}>
+                  <label>
+                    <TextareaAutosize
+                      value={response}
+                      onChange={(e) => {
+                        const updatedResponses = [...responses]
+                        updatedResponses[index] = e.target.value
+                        setResponses(updatedResponses)
+                      }}
+                      maxRows={20}
+                      minRows={1}
+                      cols={50}
+                    />
+                  </label>
+                  <button type="submit">Generate</button>
+                  {/* <button>Save</button> */}
+                  <button type="button" onClick={(e) => handleCopy(e, index)}>
+                    {isCopied && index === copiedIndex
+                      ? 'Copied!'
+                      : 'Copy Text'}
+                  </button>
+                </form>
+              </div>
+            ))}
+        </div>
+
+        <div className="chatbox-prompt">
+          <form onSubmit={(e) => handleSubmit(e, -1)}>
+            <label>
+              <TextareaAutosize
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                className="chatbox-prompt--input"
+                maxRows={10}
+                minRows={1}
+              />
+            </label>
+            <button type="submit">Generate</button>
+          </form>
+        </div>
       </div>
     </>
   )
