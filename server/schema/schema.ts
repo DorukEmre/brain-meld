fd59b31cff0f2ea1fe91072d1aa022df3543077b
DorukEmre <doruk.emre@outlook.com>
2023-02-23 13:37:48 +0000

    Created GraphQL schema to fetch n mutate Tree Node

--- server/schema/schema.ts
+++ server/schema/schema.ts
@@ -2,16 +2,59 @@ import { createSchema } from 'graphql-yoga'
 
 import getOpenAI from '../middleware/openai.js'
 
+import TreeNode from '../models/TreeNode.model.js'
+
 export const schema = createSchema({
   typeDefs: /* GraphQL */ `
+    type TreeNode {
+      id: Int!
+      parent: Int!
+      droppable: Boolean!
+      text: String!
+    }
     type Query {
       generateText(prompt: String!): String
+      allTreeNodes: [TreeNode]
+    }
+    type Mutation {
+      addTreeNode(
+        id: Int!
+        parent: Int!
+        droppable: Boolean!
+        text: String!
+      ): TreeNode
     }
   `,
   resolvers: {
     Query: {
       // args is the second parameter
       generateText: async (_, { prompt }) => getOpenAI(_, { prompt }),
+      allTreeNodes: () => TreeNode.find(),
+    },
+    Mutation: {
+      addTreeNode: async (_, { id, parent, droppable, text }) => {
+        const treeNode = new TreeNode({ id, parent, droppable, text })
+        await treeNode.save()
+        return treeNode
+      },
     },
   },
 })
+
+// query {
+//   allTreeNodes {
+//     id
+//     parent
+//     droppable
+//     text
+//   }
+// }
+
+// mutation {
+//   addTreeNode(id: 1, parent: 0, droppable: true, text: "New TreeNode") {
+//     id
+//     parent
+//     droppable
+//     text
+//   }
+// }
