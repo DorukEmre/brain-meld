d98dd6369fc0f09479ec6830b6daf96560a5928f
DorukEmre <doruk.emre@outlook.com>
2023-03-28 17:34:51 +0100

    Removed unused next-auth

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,31 +1,24 @@
 import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
 import client from '@/config/apolloClient'
-// import { SessionProvider } from 'next-auth/react'
 
 import Layout from '@/components/Layout'
 
 import '@/styles/reset.css'
 import '@/styles/globals.css'
 
-export default function App({
-  Component,
-  // pageProps: { session, ...pageProps },
-  pageProps,
-}: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       {/* <style jsx global>{`
         :root {
         }
       `}</style> */}
-      {/* <SessionProvider session={session}> */}
       <ApolloProvider client={client}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </ApolloProvider>
-      {/* </SessionProvider> */}
     </>
   )
 }
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,15 +1,10 @@
-// import { useSession, signIn, signOut } from 'next-auth/react'
-
 import HTMLMetaTags from '@/components/HTMLMetaTags'
 import Explorer from '@/components/Explorer'
 
 const Home = () => {
-  // const { data: session } = useSession()
-
   return (
     <>
       <HTMLMetaTags />
-      {/* {session ? <Explorer /> : <p>Not signed in</p>} */}
       <Explorer />
     </>
   )
