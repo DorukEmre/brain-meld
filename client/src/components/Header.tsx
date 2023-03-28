5606352660bcda3280eaf7e02c94fe90fefc96aa
DorukEmre <doruk.emre@outlook.com>
2023-03-28 17:38:51 +0100

    To set up authentication with next-auth

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,4 +1,24 @@
+// import { useSession, signIn, signOut } from 'next-auth/react'
+
 const Header = () => {
+  // const { data: session } = useSession()
+
+  // if (session) {
+  //   return (
+  //     <header>
+  //       Signed in as {session.user!.email} <br />
+  //       <button onClick={() => signOut()}>Sign out</button>
+  //     </header>
+  //   )
+  // } else {
+  //   return (
+  //     <header>
+  //       Not signed in <br />
+  //       <button onClick={() => signIn()}>Sign in</button>
+  //     </header>
+  //   )
+  // }
+
   return <header>Header</header>
 }
 
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,24 +1,31 @@
 import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
 import client from '@/config/apolloClient'
+// import { SessionProvider } from 'next-auth/react'
 
 import Layout from '@/components/Layout'
 
 import '@/styles/reset.css'
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  // pageProps: { session, ...pageProps },
+  pageProps,
+}: AppProps) {
   return (
     <>
       {/* <style jsx global>{`
         :root {
         }
       `}</style> */}
+      {/* <SessionProvider session={session}> */}
       <ApolloProvider client={client}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </ApolloProvider>
+      {/* </SessionProvider> */}
     </>
   )
 }
